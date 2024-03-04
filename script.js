13ae02d30f86a992baa43255c5c450611b4869cd
Filipe Andrade <filipeyay@gmail.com>
2024-03-04 22:14:44 +0100

    basic mechanics update
    Restart system added
    Restart button added
    Highscore sytem added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// TODO: Restart System and Highscore System
+// TODO: remove game over alert
 
-let blockSize = 30;
+let blockSize = 25;
 let totalRow = 18;
 let totalCol = 18;
 let board;
@@ -11,9 +11,18 @@ let speedX = 0;
 let speedY = 0;
 let foodX;
 let foodY;
+let score = 0;
+let highscore = document.getElementById("highscore");
 let snakeBody = [];
 
 let gameOver = false;
+
+// score
+if (localStorage.getItem("snakescore")) {
+  highscore.innerText = localStorage.getItem("snakescore");
+} else {
+  highscore.innerText = 0;
+}
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -30,14 +39,22 @@ function update() {
   if (gameOver) {
     return;
   }
+
+  // board
   context.fillStyle = "#23201a";
   context.fillRect(0, 0, board.width, board.height);
 
+  // food
   context.fillStyle = "#792639";
   context.fillRect(foodX, foodY, blockSize, blockSize);
 
   if (snakeX == foodX && snakeY == foodY) {
     snakeBody.push([foodX, foodY]);
+    score += 1;
+    document.getElementById("currentscore").innerText = score;
+    if (score >= localStorage.getItem("snakescore")) {
+      localStorage.setItem("snakescore", score);
+    }
     placeFood();
   }
 
@@ -49,6 +66,7 @@ function update() {
     snakeBody[0] = [snakeX, snakeY];
   }
 
+  // snake
   context.fillStyle = "#868645";
   snakeX += speedX * blockSize;
   snakeY += speedY * blockSize;
@@ -74,6 +92,7 @@ function update() {
   }
 }
 
+// controls
 function changeDirection(e) {
   if (e.code == "ArrowUp" && speedY != 1) {
     speedX = 0;
@@ -94,3 +113,10 @@ function placeFood() {
   foodX = Math.floor(Math.random() * totalCol) * blockSize;
   foodY = Math.floor(Math.random() * totalRow) * blockSize;
 }
+
+// Restart
+let restart = document
+  .getElementById("restart")
+  .addEventListener("click", function () {
+    location.reload();
+  });
